e933c43f5a9182859c3756ed9aae387c6d715c4e
double Z <doubleZ0108@163.com>
2020-04-26 16:35:47 +0800

    :bug:fix multi unordered list

--- markdown2mindnode.py
+++ markdown2mindnode.py
@@ -8,14 +8,10 @@ def markdown_refactor_to_mindnode(file):
     refactor_strs = []
     with open(file, mode='r', encoding='utf-8') as f:
         for line in f :
-            first_ch = line[0]
-            if first_ch == '-':
-                newline =' ' + line[1:]
-                newline = newline.lstrip()
-                refactor_strs.append(newline)
-            elif first_ch == ' ':
-                newline = line.replace('-', ' ')
-                refactor_strs.append(newline)
+            list_symbol_index = line.find('-')
+            num_of_space = list_symbol_index * 2
+            newline = ''.join([' '] * num_of_space) + line.replace('-', ' ').lstrip()
+            refactor_strs.append(newline)
     return refactor_strs
 
 def save_to_clipboard(file):
@@ -40,7 +36,7 @@ def save_as_refactored_file(file):
 if __name__ == '__main__':
     file = '#'
     try:
-        if len(sys.argv) > 3:   # 文件名中有空格
+        if len(sys.argv) > 3:   # there is Space in filename
             if sys.argv[-1] in ('file', 'clipboard'):
                 file = ' '.join(sys.argv[1:-1])
             else:
@@ -53,8 +49,7 @@ if __name__ == '__main__':
         if file != '#':
             try:
                 mode = sys.argv[-1]
-                print(mode)
-                if mode.find('.md') != -1:
+                if mode.find('.md') != -1:  # actually don't provide the second param
                     raise Exception
             except Exception:
                 mode = 'clipboard'
@@ -67,3 +62,5 @@ if __name__ == '__main__':
                 else:
                     print("The second parameter option is [clipboard | file], copy to clipboard by default.")
                     save_to_clipboard(file)
+    # file = '/Users/doublez/Downloads/README.md'
+    # markdown_refactor_to_mindnode(file)
